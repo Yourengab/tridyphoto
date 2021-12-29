d74c1d8c264654991a9ff18a3fe0efffc8b69745
Yourengab <yourenw1@gmail.com>
2021-12-29 23:02:30 +0800

    Upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const menuToggle = document.querySelector(".menu-toggle input");
+const nav = document.querySelector("nav ul");
+menuToggle.addEventListener("click", function () {
+  nav.classList.toggle("slide");
+});
